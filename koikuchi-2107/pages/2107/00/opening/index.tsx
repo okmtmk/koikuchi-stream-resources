7608ded96a59b2dc339f27f8e2946427d7ca0285
okmtmk <c0118060b4@edu.teu.ac.jp>
2021-07-01 21:51:36 +0900

    全員分のスプラッシュ

--- koikuchi-2107/pages/2107/00/opening/index.tsx
+++ koikuchi-2107/pages/2107/00/opening/index.tsx
@@ -4,6 +4,11 @@ import styles from "./index.module.scss";
 import logopng from "../../../../public/images/licensed/logo/logo_white.png";
 import mushPic from "../../../../public/images/licensed/artists/2107/mush_clip.png"
 import jitPic from "../../../../public/images/licensed/artists/2107/ji__t_clip.png"
+import mimoPicCliped from "../../../../public/images/licensed/artists/2107/okmt_clip.png"
+import mimoPic from "../../../../public/images/licensed/artists/2107/okmt.png"
+import wiredlainPic from "../../../../public/images/licensed/artists/2107/wiredlain.jpg"
+import xaiPicCliped from "../../../../public/images/licensed/artists/2107/xai_clip.png"
+import xaiPic from "../../../../public/images/licensed/artists/2107/xai_noedit.jpg"
 
 const index = () => (
   <div className={styles.container}>
@@ -86,6 +91,59 @@ const index = () => (
           </div>
         </div>
 
+        <div className={styles.mimoContainer}>
+          <div className={styles.mimoNameContainer}>
+            <div className={styles.mimoName}>
+              <div>3rd</div>
+              <div>おかもと</div>
+            </div>
+          </div>
+
+          <div className={styles.mimoImgContainer}>
+            <div className={styles.mimoImg}>
+              <div>
+                <Image src={mimoPicCliped} width={800} height={927} objectFit="contain" />
+              </div>
+              <div>
+                <Image src={mimoPic} width={1440} height={1080} objectFit="cover" />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className={styles.wiredlainContainer}>
+          <div className={styles.wiredlainMainArea}>
+            <div className={styles.wiredlainIcon}>
+              <Image src={wiredlainPic} width={400} height={400} objectFit="cover" />
+            </div>
+            <div className={styles.wiredlainName}>
+              <div>4th</div>
+              <div>WiredLain</div>
+            </div>
+          </div>
+
+          <div className={styles.wiredlainBackgroundContainer}>
+            <div className={styles.wiredlainBackground}>
+              <Image src={wiredlainPic} width={1440} height={1080} objectFit="cover" />
+            </div>
+          </div>
+        </div>
+
+        <div className={styles.xaiContainer}>
+          <div className={styles.xaiName}>
+            <div>Last</div>
+            <div>xai</div>
+          </div>
+          <div className={styles.xaiImageContainer}>
+            <div>
+              <Image src={xaiPicCliped} width={1440} height={1080} objectFit="contain" />
+            </div>
+            <div>
+              <Image src={xaiPic} width={1440} height={1080} objectFit="cover" />
+            </div>
+          </div>
+        </div>
+
       </div>
     </div>
     <div className={styles.logoarea}>
@@ -99,7 +157,10 @@ const index = () => (
             alt="こいくちストリーム"
           />
         </div>
-        <div>Start 21:00</div>
+        <div>
+          <div>START</div>
+          <div>21:00</div>
+        </div>
       </div>
     </div>
   </div>
